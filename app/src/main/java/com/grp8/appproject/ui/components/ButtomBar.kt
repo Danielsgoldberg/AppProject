c8b4f55b796e85d3c8fedf0f8a402bdbb813ba81
Daniel Goldberg <202108237@post.au.dk>
2024-04-11 14:44:36 +0200

    Skubbe skubbe skub til koden

--- app/src/main/java/com/grp8/appproject/ui/components/ButtomBar.kt
+++ app/src/main/java/com/grp8/appproject/ui/components/ButtomBar.kt
@@ -12,46 +12,52 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.grp8.appproject.ui.screens.Home
 import com.grp8.appproject.ui.theme.AppProjectTheme
+import java.time.format.TextStyle
 
 @Composable
-fun BottomBar(modifier: Modifier = Modifier, navigate:() -> Unit) {
+fun BottomBar(modifier: Modifier = Modifier, navigate:(screen: String) -> Unit) {
     Row(
         modifier = modifier.then(
             Modifier
                 .fillMaxWidth()
                 .height(56.dp) // Adjust the height as needed
-                .background(Color.White)
+                .background(Color.Transparent)
         ),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceAround // Distribute icons evenly
     ) {
-        IconButton(onClick = navigate) {
+        IconButton(onClick = {navigate("Search")}) {
             Icon(
                 imageVector = Icons.Default.Search,
                 contentDescription = "Search",
-                tint = Color.Black
+                tint = Color.White
             )
         }
-        IconButton(onClick = navigate) {
+        IconButton(onClick = {navigate("Home")} ) {
             Icon(
                 imageVector = Icons.Default.Home,
                 contentDescription = "Home",
-                tint = Color.Black
+                tint = Color.White
             )
         }
-        IconButton(onClick = navigate) {
+        IconButton(onClick = {navigate("Profile")} ) {
             Icon(
                 imageVector = Icons.Default.AccountCircle,
                 contentDescription = "Account",
-                tint = Color.Black
+                
+                tint = Color.White
             )
+
         }
     }
 }

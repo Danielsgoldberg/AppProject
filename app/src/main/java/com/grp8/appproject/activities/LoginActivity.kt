a346585f5dea124d9c4b5c676c50774a5a065731
nikolinehoeyiversen <nikohoey@gmail.com>
2024-04-11 13:57:47 +0200

    loginactivity

--- app/src/main/java/com/grp8/appproject/activities/LoginActivity.kt
+++ app/src/main/java/com/grp8/appproject/activities/LoginActivity.kt
@@ -12,22 +12,22 @@ import com.grp8.appproject.integrations.firestore.authentication.BasicAuthClient
 import com.grp8.appproject.integrations.firestore.authentication.Login
 import com.grp8.appproject.ui.screens.Cocktails
 import com.grp8.appproject.ui.theme.AppProjectTheme
-class LoginActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-
-        val service = BasicAuthClient();
-        val nav = NavController(title="", navigationText = "");
-        super.onCreate(savedInstanceState)
-        setContent {
-            AppProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Login(service, nav)
-                }
-            }
-        }
-    }
-}
+//class LoginActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//
+//        val service = BasicAuthClient();
+//        val nav = NavController(title="", navigationText = "");
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            AppProjectTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Login(service, nav)
+//                }
+//            }
+//        }
+//    }
+//}

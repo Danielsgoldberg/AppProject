887d02722ab4b6c1125c51de693edd7a065790f9
Camilla Kolby Thomsen <camillakthomsen@hotmail.com>
2024-05-12 16:05:12 +0200

    opsætning af profil

--- app/src/main/java/com/grp8/appproject/ui/screens/Profile.kt
+++ app/src/main/java/com/grp8/appproject/ui/screens/Profile.kt
@@ -1,43 +1,43 @@
 package com.grp8.appproject.ui.screens
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.grp8.appproject.integrations.firestore.authentication.BasicAuthClient
+import kotlinx.coroutines.launch
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.grp8.appproject.R
-import com.grp8.appproject.integrations.firestore.authentication.BasicAuthClient
-import kotlinx.coroutines.launch
 
 
 @Composable
-fun Profile(findfavorites: () -> Unit, service: BasicAuthClient, cancel: () -> Unit) {
+fun Profile(findfavorites:() -> Unit, service: BasicAuthClient, cancel:() -> Unit) {
     val scope = rememberCoroutineScope()
     val username = Firebase.auth.currentUser?.email
     Column(
